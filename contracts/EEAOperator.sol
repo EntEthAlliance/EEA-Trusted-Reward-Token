22172c9c30d1d0ce0c9c057695e815cc9d9445fd
Antonina Norair <antonina.norair@gmail.com>
2019-07-01 12:33:29 -0700

    Add meaningful constructor to an eea operator

--- contracts/EEAOperator.sol
+++ contracts/EEAOperator.sol
@@ -1,9 +1,27 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+import "./RewardToken.sol";
+import "./PenaltyToken.sol";
 
 /**
  * @title EEA Operator contract
  */
-contract EEAOperator {
+contract EEAOperator is Ownable {
+
+  PenaltyToken public penaltyToken;
+  RewardToken public rewardToken;
+
+
+  constructor() public {
+    address[] memory defaultOperators = new address[](2);
+    defaultOperators[0] = address(this);
+    defaultOperators[1] = address(msg.sender);
+
+    //Create token smart contracts;
+    penaltyToken = new PenaltyToken(defaultOperators);
+    rewardToken = new RewardToken(defaultOperators);
+  }
 
 }
