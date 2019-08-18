eb73868d31a874101fc4c4c37bd58aba53e291c6
MDionSijan <MLAlonso49@Gmail.com>
2019-08-18 18:37:23 -0500

    Handles restricted EEA member transfers

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -1,17 +1,18 @@
 pragma solidity ^0.5.0;
 
-import "node_modules/openzeppelin-solidity/contracts/token/ERC777/ERC777.sol";
-
+import "./ModifiedERC777.sol";
+import "./EEATransferableToken.sol";
 
 /**
  * @title EEA Reward Token
  */
-contract RewardToken is ERC777 {
+contract RewardToken is ModifiedERC777, EEATransferableToken {
 
     /**
      * @dev Constructor that sets default operators.
      */
-    constructor(address[] memory defaultOperators) public ERC777("RewardToken", "EEART", defaultOperators) {
+    constructor(address[] memory defaultOperators, address _didRegistry, address _claimsRegistry, address _eeaIssuer)
+    public ModifiedERC777("RewardToken", "EEART", defaultOperators) EEATransferableToken( _didRegistry, _claimsRegistry, _eeaIssuer){
     }
 
     /**
@@ -22,6 +23,29 @@ contract RewardToken is ERC777 {
     function operatorMint(address account, uint256 amount, bytes calldata data, bytes calldata operatorData) external {
        require(msg.sender != account, "Error: caller cannot be holder");
        require(isOperatorFor(msg.sender, account), "ERC777: caller is not an operator for holder");
-      _mint(msg.sender, account, amount, data, operatorData);
+       _mint(msg.sender, account, amount, data, operatorData);
     }
+
+    /// RESTRICTED METHODS BETWEEN EEA MEMBERS ONLY
+
+
+
+    function send(address recipient, uint256 amount, bytes memory data) public {
+        require (_memberCheck(recipient), "Error: Not EEA member");
+        send(recipient, amount, data);
+    }
+
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require (_memberCheck(recipient), "Error: Not EEA member");
+        transfer(recipient, amount);
+    }
+
+
+
+    function transferFrom(address holder, address recipient, uint256 amount) public returns (bool) {
+        require (_memberCheck(recipient), "Error: Not EEA member");
+        transferFrom(holder, recipient, amount);
+    }
+
 }
