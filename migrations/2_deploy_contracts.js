a4478933769a733d4b10de475c946d4313735b4c
MDionSijan <MLAlonso49@Gmail.com>
2019-08-19 00:49:25 -0500

    Added contracts and edited deployment method

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,42 +1,48 @@
-const EEAOperator = artifacts.require("EEAOperator");
-const RewardToken = artifacts.require("RewardToken");
-const PenaltyToken = artifacts.require("PenaltyToken");
-const ReputationToken = artifacts.require("ReputationToken");
+const EEAOperator = artifacts.require('../contracts/EEAOperator.sol');
+const RewardToken = artifacts.require('../contracts/RewardToken.sol');
+const PenaltyToken = artifacts.require('../contracts/PenaltyToken.sol');
+const ReputationToken = artifacts.require('../contracts/ReputationToken.sol');
+const DidRegistryContract = artifacts.require('../contracts/EthereumDIDRegistry.sol');
+const EEAClaimsIssuer = artifacts.require('../contracts/EEAClaimsIssuer.sol');
+const EthereumClaimsRegistry = artifacts.require('../contracts/EthereumClaimsRegistry.sol');
 
-const DidRegistryContract = require('ethr-did-registry');
-const Contract = require("truffle-contract");
-const Web3 = require('web3');
-const truffleConfig = require("../truffle-config");
+const Contract = require('../node_modules/truffle-contract');
+const Web3 = require('../node_modules/web3');
+const truffleConfig = require('../truffle-config');
 
-require('openzeppelin-test-helpers/configure')({ web3 });
-const { singletons } = require('openzeppelin-test-helpers');
+require('../node_modules/openzeppelin-test-helpers/configure')({ web3 });
+const {singletons} = require('../node_modules/openzeppelin-test-helpers');
 
 module.exports = async function (deployer, network, accounts) {
-  let eeaAdmin = accounts[0];
+    let eeaAdmin = accounts[0];
+    /* Test
+    if (network === 'dev' || network === 'kaleido') {
+        // In a test environment an ERC777 token requires deploying an ERC1820 registry
+        await singletons.ERC1820Registry(eeaAdmin);
+    }
+    
 
-  if (network === 'dev' || network == 'kaleido') {
-    // In a test environment an ERC777 token requires deploying an ERC1820 registry
+    let DidReg = Contract(DidRegistryContract);
+
+    if (network === 'dev') {
+        DidReg.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+    }
+
+    if (network === 'kaleido') {
+        DidReg.setProvider(truffleConfig.networks.kaleido.provider());
+    }
+    */
+
     await singletons.ERC1820Registry(eeaAdmin);
-  }
+    let didReg = await deployer.deploy(DidRegistryContract);
+    let claimsReg = await deployer.deploy(EthereumClaimsRegistry);
+    let eeaIssuer = await deployer.deploy(EEAClaimsIssuer, claimsReg.address, {from: eeaAdmin});
 
-  let DidReg = Contract(DidRegistryContract);
-
-  if (network === 'dev') {
-    DidReg.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
-  }
-
-  if (network === 'kaleido') {
-    DidReg.setProvider(truffleConfig.networks.kaleido.provider());
-  }
-  let didReg = await DidReg.new({from: eeaAdmin});
-
-
-  let operator = await deployer.deploy(EEAOperator, 1, 1, {from: eeaAdmin});
-
-  let tokenOperators = [eeaAdmin, operator.address];
-  let penaltyToken = await deployer.deploy(PenaltyToken, tokenOperators);
-  let rewardToken = await deployer.deploy(RewardToken, tokenOperators);
-  let reputationToken = await deployer.deploy(ReputationToken, tokenOperators);
-
-  await operator.registerTokens(penaltyToken.address, rewardToken.address, reputationToken.address);
+    let operator = await deployer.deploy(EEAOperator, didReg.address, claimsReg.address, eeaIssuer.address, {from: eeaAdmin});
+    let tokenOperators = [eeaAdmin, operator.address];
+    let penaltyToken = await deployer.deploy(PenaltyToken, tokenOperators);
+    let reputationToken = await deployer.deploy(ReputationToken, tokenOperators);
+    let rewardToken = await deployer.deploy(RewardToken, tokenOperators, didReg.address, claimsReg.address, eeaIssuer.address);
+    await operator.registerTokens(penaltyToken.address, rewardToken.address, reputationToken.address);
+    
 };
