4dac6b6867f3426f56ba13a95086f1c3981709f7
Hadrien Croubois <hc@iex.ec>
2019-09-09 14:03:31 +0200

    update operator.py
    needs export WEB3_PROVIDER_URI= to start

--- tee/operator.py
+++ tee/operator.py
@@ -4,9 +4,9 @@ import argparse
 import json
 import re
 import os
-import web3
-from web3          import Web3, HTTPProvider
-from web3.contract import Contract
+from web3.auto        import w3
+from web3.contract    import Contract
+from web3.middleware  import construct_sign_and_send_raw_middleware
 
 # Types → Success → Value (positive values are reward, negative values are penalty)
 businessRules = {
@@ -16,16 +16,16 @@ businessRules = {
 	3: { True: +25, False: -10},
 }
 
-didToAddr = lambda did: Web3.toChecksumAddress(re.search('^did:ethr:[0-9a-f]{0,24}([0-9a-f]{40})$', did).group(1))
+didToAddr = lambda did: w3.toChecksumAddress(re.search('^did:ethr:[0-9a-f]{0,24}([0-9a-f]{40})$', did).group(1))
 
 class EEAOperator:
 	def __init__(self, config):
-		self.contract = Web3(HTTPProvider(config.gateway)).eth.contract(        \
+		self.contract = w3.eth.contract(                                        \
 			address=config.address,                                             \
-			abi=json.load(open(f'{config.contracts}/EEAOperator.json'))['abi'], \
-			ContractFactoryClass=Contract,                                      \
+			abi=json.load(open(f'{config.abis}/EEAOperator.json'))['abi'], \
 		)
-		self.account = web3.Account.privateKeyToAccount(os.environ['enclave_key'])
+		self.account = w3.eth.account.from_key(os.environ['enclave_key'])
+		w3.middleware_onion.add(construct_sign_and_send_raw_middleware(self.account))
 
 	def issue_burn_tokens(self, data):
 		organizationID = didToAddr(data['organization_ID'])
@@ -59,10 +59,9 @@ class EEAOperator:
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--input',     type=str, default='/encryptedInputs/eeaToken.json')
-	parser.add_argument('--gateway',   type=str, default='http://localhost:8545')
-	parser.add_argument('--contracts', type=str, default='.')
-	parser.add_argument('--address',   type=str, default='0xd4c2F43544e2453b99770c1188A039E7bee4Ac41')
+	parser.add_argument('--input',   type=str, default='/encryptedInputs/eeaToken.json')
+	parser.add_argument('--abis',    type=str, default='.')
+	parser.add_argument('--address', type=str, default='0xE778DC1eC36aa3625A99AF70318c5c78f8c8ae32')
 	config = parser.parse_args()
 
 	eeaoperator = EEAOperator(config)
@@ -71,5 +70,7 @@ if __name__ == '__main__':
 		[ key, raw ] = re.search('(\w*)\[\]:(.*)', file.read()).groups()
 	try:
 		getattr(eeaoperator, key)(json.loads(raw))
-	except NotImplementedError:
-		print(f'[Error] {key} is not implemented')
+	except NotImplementedError as e:
+		print(f'[Error] {e}')
+	except AttributeError as e:
+		print(f'[Error] {e}')
